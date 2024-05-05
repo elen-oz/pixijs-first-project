e95266c6300772d14f0e637ff3accb346c950955
elen-oz <ozerova.lenka@gmail.com>
2024-05-05 15:13:02 +0200

    feat: add intersctivity: pointerdown

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-// import { Application, Graphics } from './pixi.mjs';
-import * as PIXI from './pixi.mjs';
+import { Application, Graphics, Rectangle } from './pixi.mjs';
 import { assetsMap } from './assetsMap.js';
 import { Tank } from './Tank.js';
 
 // Create the application
-const app = new PIXI.Application({
+const app = new Application({
   width: 800,
   height: 800,
   backgroundColor: 0xc2c2c2,
@@ -12,7 +11,7 @@ const app = new PIXI.Application({
 });
 
 const runGame = () => {
-  const marker = new PIXI.Graphics();
+  const marker = new Graphics();
   marker.beginFill(0xff0000, 1);
   marker.drawCircle(0, 0, 5);
   marker.endFill();
@@ -21,9 +20,24 @@ const runGame = () => {
   app.stage.addChild(tank.view);
   app.stage.addChild(marker);
 
+  app.stage.position.set(800 / 2, 800 / 2);
+
   window['TANK'] = tank;
 
-  app.stage.position.set(800 / 2, 800 / 2);
+  const pointerdown = ({ data }) => {
+    console.log(data);
+
+    const positions = data.getLocalPosition(app.stage);
+    app.stage.addChild(
+      new Graphics()
+        .beginFill(0x8ff559, 1)
+        .drawCircle(positions.x, positions.y, 8)
+    );
+  };
+  app.stage.on('pointerdown', pointerdown, undefined);
+  app.stage.interactive = true;
+  app.stage.interactiveChildren = false;
+  app.stage.hitArea = new Rectangle(-400, -400, 800, 800);
 };
 
 assetsMap.sprites.forEach((value) => app.loader.add(value));
